a80f6dcd082c21ddca2a55ade3648ec92c2e8e30
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-14 20:23:05 -0300

    Requisito 3 corrigido

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -2,7 +2,7 @@ const generateToken = () => {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const getRandomNum = (max) => Math.floor(Math.random() * max) + 1;
   const tokenArray = [];
-  for (let i = 1; i <= 16; i += 1) tokenArray.push(characters[getRandomNum(characters.length)]);
+  for (let i = 0; i < 16; i += 1) tokenArray.push(characters[getRandomNum(characters.length)]);
   const token = tokenArray.join('');
   return token;
 };
